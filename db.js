207e41df6adb12554a0aebd245f8c74ca6a61c2b
maczin13f <154028448+maczin13f@users.noreply.github.com>
2025-05-22 18:07:48 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function conectarMongoDB() {
   try {
-    await mongoose.connect("mongodb+srv://<usuario>:<senha>@<cluster>.mongodb.net/<nome-do-banco>?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://felipeapi:felipe2503@testgoogleapi.o6wmra8.mongodb.net/?retryWrites=true&w=majority&appName=testgoogleapi");
     console.log("🟢 MongoDB conectado com sucesso!");
   } catch (erro) {
     console.error("🔴 Erro ao conectar no MongoDB:", erro);
